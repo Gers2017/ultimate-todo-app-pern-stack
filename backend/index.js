985ad54ceaec669bf21b661c8a4e02dec15f11b4
Gerson <Gers2017@outlook.com>
2021-04-10 00:19:52 -0600

    frontend setup

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,10 @@ app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1)",
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
-    res.json({ result: "Todo was added!" });
+    res.json(newTodo.rows[0]); //returning value
   } catch (error) {
     console.error(error);
   }
